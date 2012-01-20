8751d6517ca209975b2665975493056b19c18f80
Ilya Bogdanovich <bogdanovichiy@yandex-team.ru>
2012-01-20 18:54:27 +0400

    Fixed warnings & messages

--- GitFlowCommon.pm
+++ GitFlowCommon.pm
@@ -24,15 +24,15 @@ my $encoded_password = trim(`git config --get gitflow.ld.password`);
 my $password         = trim(decode_base64($encoded_password));
 my $jira_base_url    = 'http://jira/';
 
-my $workflow_statuses = {
+my %workflow_statuses = (
 	'Open' => 1,
 	'In Progress' => 3,
 	'Code Review' => 10011,
 	'Resolved' => 5,
 	'Closed' => 6
-};
+);
 
-my $workflow_resolutions = {
+my %workflow_resolutions = (
 	'Fixed' => 1,
 	'Won\'t Fix' => 2,
 	'Duplicate' => 3,
@@ -40,7 +40,7 @@ my $workflow_resolutions = {
 	'Can\'t Reproduce' => 5,
 	'Delayed' => 6,
 	'Invalid' => 10
-};
+);
 
 my $jira = JIRA::Client->new( $jira_base_url, $user, $password );
 
@@ -51,19 +51,23 @@ my $jira = JIRA::Client->new( $jira_base_url, $user, $password );
 #print Dumper($jira->get_statuses());
 
 sub warnTicketInvalid() {
-	die "Feature name is not a valid jira-ticket.\n";
+	die "\nERROR: Feature name is not a valid jira-ticket.\n\n";
 }
 
 sub warnTicketAlreadyWorking() {
-	print "Feature is already in progress.\n";
+	print "\nWARNING! Feature is already in progress.\n\n";
+}
+
+sub warnTicketDone() {
+	print "\nWARNING! You are trying to work on finished ticket.\n\n";
 }
 
 sub warnTicketNotWorking() {
-	print "Feature is not in progress.\n";
+	print "\nWARNING! You are not working on this feature by now.\n\n";
 }
 
 sub warnTicketNotFinished() {
-	print "Feature is not done yet.\n";
+	print "\nWARNING! You are finishing feature that is not resolved yet.\n\n";
 }
 
 # start new feature
@@ -75,7 +79,7 @@ sub startFeature {
 		# get the current ticket
 		my $curIssue = eval { $jira->getIssue($curFeature) };
 		# move issue over the workflow (stop progress)
-		if ($curIssue->{status} == GitFlowCommon::$workflow_statuses{'In Progress'}) {
+		if ($curIssue->{status} == $workflow_statuses{'In Progress'}) {
 			$jira->progress_workflow_action_safely(
 				$curIssue,
 				'Stop Progress'
@@ -89,17 +93,20 @@ sub startFeature {
 	# check tiket exists
 	warnTicketInvalid() if $@;
 	# check ticket is opened
-	warnTicketAlreadyWorking() if ($newIssue->{status} != GitFlowCommon::$workflow_statuses{'Open'});
+	warnTicketAlreadyWorking() if ($newIssue->{status} == $workflow_statuses{'In Progress'});
+	warnTicketDone() if ($newIssue->{status} != $workflow_statuses{'Open'} && $newIssue->{status} != $workflow_statuses{'In Progress'});
 	
 	# remember ticket & assignee 
 	system("git config gitflow.prefix.feature.issue $newFeature");
 	system("git config gitflow.prefix.feature.assignee $assignee");
 	
-	# move issue over the workflow (start progress)
-	$jira->progress_workflow_action_safely(
-		$newIssue,
-		'Start Progress'
-	);
+	if ($newIssue->{status} == $workflow_statuses{'Open'}) {
+		# move issue over the workflow (start progress)
+		$jira->progress_workflow_action_safely(
+			$newIssue,
+			'Start Progress'
+		);
+	}
 }
 
 # finish current feature
@@ -111,7 +118,7 @@ sub finishFeature {
 	# check tiket exists
 	warnTicketInvalid() if $@;
 	# check ticket is resolved
-	warnTicketNotFinished() if ($issue->{status} != eval {GitFlowCommon::$workflow_statuses{'Resolved'}} && $issue->{status} != eval {GitFlowCommon::$workflow_statuses{'Closed'}});
+	warnTicketNotFinished() if ($issue->{status} != $workflow_statuses{'Resolved'} && $issue->{status} != $workflow_statuses{'Closed'});
 	
 	# clear config
 	system("git config --unset gitflow.prefix.feature.issue");
